bd59d62bbf727e1bd50ddeaf45a9867ede19d7e7
rfoe <rfoe@10.208.71.196>
2018-06-04 13:49:59 +0800

    add secure channel

--- fisdemoaggregator/src/main/java/com/redhat/fisdemoaggregator/Application.java
+++ fisdemoaggregator/src/main/java/com/redhat/fisdemoaggregator/Application.java
@@ -15,10 +15,28 @@
  */
 package com.redhat.fisdemoaggregator;
 
+import java.io.InputStream;
+
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.descriptor.web.LoginConfig;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+//import org.keycloak.adapters.HttpFacade;
+//import org.keycloak.adapters.KeycloakConfigResolver;
+//import org.keycloak.adapters.KeycloakDeployment;
+//import org.keycloak.adapters.KeycloakDeploymentBuilder;
+//import org.keycloak.adapters.tomcat.KeycloakAuthenticatorValve;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+
+
 
 @SpringBootApplication
 @ImportResource({"classpath:spring/camel-context.xml"})
@@ -29,6 +47,75 @@ public class Application extends SpringBootServletInitializer {
     }
 
    
+    
+    
+//    @Bean
+//    public EmbeddedServletContainerCustomizer getKeycloakContainerCustomizer() {
+//        return new EmbeddedServletContainerCustomizer() {
+//            @Override
+//            public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+//            	
+//            	System.err.println("@@ TomcatEmbeddedServletContainerFactory " + configurableEmbeddedServletContainer.toString());
+//            	
+//                if (configurableEmbeddedServletContainer instanceof TomcatEmbeddedServletContainerFactory) {
+//                		System.err.println("@@ TomcatEmbeddedServletContainerFactory");
+//                    TomcatEmbeddedServletContainerFactory container = (TomcatEmbeddedServletContainerFactory) configurableEmbeddedServletContainer;
+//
+//                    container.addContextValves(new KeycloakAuthenticatorValve());
+//
+//                    container.addContextCustomizers(getKeycloakContextCustomizer());
+//                }
+//            }
+//        };
+//    }
+//
+//    @Bean
+//    public TomcatContextCustomizer getKeycloakContextCustomizer() {
+//        return new TomcatContextCustomizer() {
+//            @Override
+//            public void customize(Context context) {
+//                LoginConfig loginConfig = new LoginConfig();
+//                
+//                System.err.println("At Context Customiser");
+//                loginConfig.setAuthMethod("KEYCLOAK");
+//                context.setLoginConfig(loginConfig);
+//
+//                context.addSecurityRole("demo");
+//
+//                SecurityConstraint constraint = new SecurityConstraint();
+//                constraint.addAuthRole("demo");
+//
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/demo");
+//                constraint.addCollection(collection);
+//
+//                context.addConstraint(constraint);
+//
+//                context.addParameter("keycloak.config.resolver", SpringBootKeycloakConfigResolver.class.getName());
+//            }
+//        };
+//    }
+//
+//    public static class SpringBootKeycloakConfigResolver implements KeycloakConfigResolver {
+//
+//        private KeycloakDeployment keycloakDeployment;
+//
+//        @Override
+//        public KeycloakDeployment resolve(HttpFacade.Request request) {
+//            if (keycloakDeployment != null) {
+//                return keycloakDeployment;
+//            }
+//
+//            InputStream configInputStream = getClass().getResourceAsStream("/keycloak.json");
+//            if (configInputStream == null) {
+//                keycloakDeployment = new KeycloakDeployment();
+//            } else {
+//                keycloakDeployment = KeycloakDeploymentBuilder.build(configInputStream);
+//            }
+//
+//            return keycloakDeployment;
+//        }
+//    }
 
    
 }
